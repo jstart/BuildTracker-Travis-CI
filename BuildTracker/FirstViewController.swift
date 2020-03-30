a1f0ee622bbfbc6b7f90cea28a60c8d8190904db
Christopher Truman <cleetruman@gmail.com>
2020-03-30 02:46:33 -0700

    Initial Commit

---
+++ BuildTracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BuildTracker
+//
+//  Created by Christopher Truman on 3/30/20.
+//  Copyright © 2020 truman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
